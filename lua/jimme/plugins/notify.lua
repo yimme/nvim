474244d36b5266ffd72de6f3ef884e4aa10b53da
Jimme <hendrixjimme@gmail.com>
2024-06-20 10:13:14 +0200

    fix notify width

--- lua/jimme/plugins/notify.lua
+++ lua/jimme/plugins/notify.lua
@@ -2,7 +2,8 @@ return {
   'rcarriga/nvim-notify',
   config = function()
     require('notify').setup {
-      background_colour = '#00000000',
+      background_colour = 'NotifyBackground',
+      render = 'wrapped-compact',
     }
     vim.notify = require 'notify'
   end,
