62f34c241885da552a9512b45222512e60e845d5
Jimme <mail@jim.me>
2025-06-14 13:04:11 +0200

    enable gopls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,7 @@ return {
   'mason-org/mason-lspconfig.nvim',
   opts = {
     ensure_installed = ensure_installed,
-    automatic_enable = { 'lua_ls', 'cssls' },
+    automatic_enable = { 'lua_ls', 'cssls', 'gopls' },
   },
   dependencies = {
     { 'mason-org/mason.nvim', opts = {
