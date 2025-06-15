18e86df40d334eae78ffe8e8e95e10ff6ae5a80b
Jimme <mail@jim.me>
2025-06-15 18:36:40 +0200

    update vue alpha to beta

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 local ensure_installed = { 'lua_ls', 'vue_ls@3.0.0-alpha.10', 'vtsls', 'cssls', 'gopls', 'pyright' }
+=======
+local ensure_installed = { 'lua_ls', 'vue_ls@3.0.0-beta.1', 'vtsls', 'cssls', 'gopls' }
+>>>>>>> Stashed changes
 
 return {
   'mason-org/mason-lspconfig.nvim',
