bf783138e25ee10292e21fbaf2c9a51d3ff5203d
Jimme <hendrixjimme@gmail.com>
2025-06-19 17:42:12 +0200

    disable telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   'nvim-telescope/telescope.nvim',
   branch = 'master',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  enabled = false,
   keys = {
     { '<leader>sf', '<cmd>Telescope find_files<cr>', { Desc = 'Search Files' } },
     { '<leader>sd', '<cmd>Telescope diagnostics<cr>', { Desc = 'Search Diagnostics' } },
