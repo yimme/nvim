fe085c57dea1baa7b8faf0a33f4c9c54b0f0e25c
Jimme <hendrixjimme@gmail.com>
2025-06-19 17:42:36 +0200

    fix nvim-lint init

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,38 +1,20 @@
 return {
   'mfussenegger/nvim-lint',
   event = { 'BufReadPre', 'BufNewFile' },
-  keys = {
-    {
-      '<leader>l',
-      function()
-        require('lint').try_lint()
-      end,
-      desc = 'Trigger linting for current buffer',
-    },
-  },
-  opts = {
-    linters_by_ft = {
+  config = function()
+    vim.env.ESLINT_D_PPID = vim.fn.getpid()
+    local lint = require 'lint'
+
+    lint.linters_by_ft = {
+
       javascript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
       typescript = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
       vue = { 'eslint_d' },
       python = { 'flake8' },
-    },
-    linters = {
-      eslint_d = {
-        args = {
-          '--no-warn-ignored',
-          function()
-            return vim.api.nvim_buf_get_name(0)
-          end,
-        },
-      },
-    },
-  },
-  config = function()
-    vim.env.ESLINT_D_PPID = vim.fn.getpid()
-    local lint = require 'lint'
+    }
+
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
