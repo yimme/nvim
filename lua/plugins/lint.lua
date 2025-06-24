8b689c74824452fb68c53450a898b3d6388d9b79
Jimme <hendrixjimme@gmail.com>
2025-06-24 15:56:21 +0200

    add oxlint for vue

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -6,12 +6,11 @@ return {
     local lint = require 'lint'
 
     lint.linters_by_ft = {
-
       javascript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
       typescript = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
-      vue = { 'eslint_d' },
+      vue = { 'eslint_d', 'oxlint' },
       python = { 'flake8' },
     }
 
