bce325395a5a9092bb34e00be791dfd8526698b5
Jimme <hendrixjimme@gmail.com>
2026-03-14 15:58:58 +0100

    fix flake8

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -12,7 +12,6 @@ return {
       typescript = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
       vue = { 'eslint_d', 'oxlint' },
-      python = { 'flake8' },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
