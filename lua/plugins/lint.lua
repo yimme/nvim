8c1cabfcf019f948dd58deff0ce628d4d570a8c9
Jimme <hendrixjimme@gmail.com>
2026-03-20 10:57:34 +0100

    fix file limit nonsense

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -5,21 +5,41 @@ return {
     vim.env.ESLINT_D_PPID = vim.fn.getpid()
     local lint = require 'lint'
 
+    local eslint_config_files = {
+      'eslint.config.js',
+      'eslint.config.mjs',
+      'eslint.config.cjs',
+      '.eslintrc.js',
+      '.eslintrc.cjs',
+      '.eslintrc.json',
+      '.eslintrc.yaml',
+      '.eslintrc.yml',
+    }
+
+    local function has_eslint_config(bufnr)
+      local bufname = vim.api.nvim_buf_get_name(bufnr)
+      local path = vim.fs.dirname(bufname)
+      return #vim.fs.find(eslint_config_files, { upward = true, path = path, type = 'file' }) > 0
+    end
+
+    local eslint_fts = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' }
+
     lint.linters_by_ft = {
       -- go = { 'golangcilint' },
-      javascript = { 'eslint_d' },
-      javascriptreact = { 'eslint_d' },
-      typescript = { 'eslint_d' },
-      typescriptreact = { 'eslint_d' },
-      vue = { 'eslint_d' },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local ft = vim.bo[bufnr].filetype
+        if vim.tbl_contains(eslint_fts, ft) and has_eslint_config(bufnr) then
+          lint.try_lint { 'eslint_d' }
+        else
+          lint.try_lint()
+        end
       end,
     })
   end,
